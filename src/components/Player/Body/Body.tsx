2e152627a75f896fe33fe4272090094c9a335eb8
Darragh Mcalone <darraghmcalone@hotmail.co.uk>
2020-10-26 15:15:39 +0000

    Add recently played row and styling

--- src/components/Player/Body/Body.tsx
+++ src/components/Player/Body/Body.tsx
@@ -2,17 +2,29 @@ import React from "react";
 import { SpotifyApiProps } from "../types";
 import Header from "./Header/Header";
 import "./Body.css";
+import { useStateValue } from "../../../store/StateProvider";
+
+type Props = {
+  images: { url: string | undefined }[];
+  name: React.ReactNode;
+};
 
 function Body({ spotify }: SpotifyApiProps) {
+  const [{ recently_played_playlists }] = useStateValue();
+  const recentlyPlayedAlbums = recently_played_playlists?.items
+    .map((item: any) => item?.track?.album)
+    .splice(0, 4);
   return (
     <div className="body">
       <Header spotify={spotify} />
-      <div className="body__info">
-        <img src="" alt="" />
-        <div className="body__infoText">
-          <strong>PLAYLIST</strong>
-          <h2>Discover Weekly</h2>
-        </div>
+      <h2>Recently played</h2>
+      <div className="body__row">
+        {recentlyPlayedAlbums?.map((item: Props) => (
+          <div className="body__block">
+            <img alt="" src={item.images[0].url} />
+            <h4>{item.name}</h4>
+          </div>
+        ))}
       </div>
     </div>
   );
