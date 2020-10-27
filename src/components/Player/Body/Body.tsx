dd9355392c5c4647381fb08d74bac3056bcf7b1a
Darragh Mcalone <darraghmcalone@hotmail.co.uk>
2020-10-27 10:23:16 +0000

    Update styling

--- src/components/Player/Body/Body.tsx
+++ src/components/Player/Body/Body.tsx
@@ -10,10 +10,22 @@ type Props = {
 };
 
 function Body({ spotify }: SpotifyApiProps) {
-  const [{ recently_played_playlists }] = useStateValue();
+  const [
+    { recently_played_playlists, new_releases_playlists },
+  ] = useStateValue();
+
   const recentlyPlayedAlbums = recently_played_playlists?.items
     .map((item: any) => item?.track?.album)
     .splice(0, 4);
+  const newReleases = new_releases_playlists
+    ?.map((item: any) => (
+      <div className="body__block">
+        <img alt="" src={item.images[0].url} />
+        <h4>{item.name}</h4>
+      </div>
+    ))
+    .splice(0, 4);
+
   return (
     <div className="body">
       <Header spotify={spotify} />
@@ -26,6 +38,11 @@ function Body({ spotify }: SpotifyApiProps) {
           </div>
         ))}
       </div>
+      <div className="body__rowTitle">
+        <h2>New releases</h2>
+        <h3>SEE ALL</h3>
+      </div>
+      <div className="body__row">{newReleases}</div>
     </div>
   );
 }
