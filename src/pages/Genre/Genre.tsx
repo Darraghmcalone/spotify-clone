55763764f9c2171e73afa3e2797bb492298bd7df
Darragh Mcalone <darraghmcalone@hotmail.co.uk>
2020-11-25 16:16:08 +0000

    Add Nav link to albums and artists

--- src/pages/Genre/Genre.tsx
+++ src/pages/Genre/Genre.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from "react";
 import "./Genre.css";
 import Header from "../../components/Player/Header/Header";
 import { RouteComponentProps } from "react-router";
+import { Link } from "react-router-dom";
 
 type Props = { component: FunctionComponent } & RouteComponentProps;
 
@@ -12,12 +13,24 @@ const Genre: FunctionComponent<Props> = ({ location }: any) => {
       <Header />
       <h1>{rowTitle}</h1>
       <div className="body__rows">
-        {rowData?.map((item: any, index: number) => (
-          <div className="body__block" key={index}>
-            <img alt="" src={item.images[0].url} />
-            <h4>{item.name}</h4>
-          </div>
-        ))}
+        {rowData?.map((item: any, index: number) => {
+          const { type = "playlist" } = item;
+          return (
+            <Link
+              to={{
+                pathname: `/${type}/${item.id}`,
+                state: {
+                  item,
+                },
+              }}
+              key={index}
+              className="body__block"
+            >
+              <img alt="" src={item.images[0].url} />
+              <h4>{item.name}</h4>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
